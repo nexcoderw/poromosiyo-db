c636b5598b770c70e48ce531d80f42deba9384eb
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-08-11 18:09:22 +0200

    Configure Poromosiyo development shadow database

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 
-import { defineConfig } from 'prisma/config';
+import { defineConfig, env } from 'prisma/config';
+
+const shadowDatabaseUrl = process.env['SHADOW_DATABASE_URL'];
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
@@ -8,6 +10,11 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: process.env['DATABASE_URL'],
+    url: env('DATABASE_URL'),
+    ...(shadowDatabaseUrl
+      ? {
+          shadowDatabaseUrl,
+        }
+      : {}),
   },
 });
